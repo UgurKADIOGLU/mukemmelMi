97cd1177605ade678c707296b3d4b1b575c69cc5
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-20 21:58:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int sayi =6;
+    int toplam=0;
+    for(int i=1;i<sayi;i++){
+        if(sayi%i==0){
+            toplam=toplam+i;
+        }
+    }
+    if(sayi==toplam){
+        System.out.println(sayi+" mükemmel bir sayıdır...");
+    }else {
+        System.out.println(sayi+" mükemmel bir sayı değildir...");
+    }
+    }
+}
